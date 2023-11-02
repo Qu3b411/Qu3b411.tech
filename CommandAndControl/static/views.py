ab98ad5b670800fc93017e3cbbe613a6936d66fa
qu3b411 <jmohrbutter@gmail.com>
2023-11-01 20:08:29 -0500

    adding about page

--- CommandAndControl/static/views.py
+++ CommandAndControl/static/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def default_view(request):
     return render(request, 'welcome.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+
